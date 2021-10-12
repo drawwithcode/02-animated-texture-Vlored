5e32dced914daad9a551232f7c2773bdc69d40d2
Vlored <90728068+Vlored@users.noreply.github.com>
2021-10-12 22:42:03 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,13 +1,22 @@
-function preload() {
-  // put preload code here
-}
-
+let angle;
+let a = 20;
+let b = 40;
+let c = 50;
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  // put setup code here
+  frameRate(30);
 }
 
 function draw() {
-  // put drawing code here
-  background("red");
+  angle = PI / 80 - (PI * ((sin(frameCount / 20) * PI) / 4)) / 100;
+  background("black");
+  translate(windowWidth / 2, windowHeight / 2);
+  stroke(a + frameCount, b, c + frameCount);
+  noFill();
+
+  for (let i = 0; i < 360; i++) {
+    let r = 8 * i * 0.8;
+    rect(-r / 2, -r / 2, r, r);
+    rotate(angle);
+  }
 }
